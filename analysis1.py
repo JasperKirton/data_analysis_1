09d49bfd0bdf21009579b7fb4709c14b8df2b52e
agapesoft <mu301fp@gold.ac.uk>
2017-11-10 20:12:06 +0000

    2 charts

--- analysis1.py
+++ analysis1.py
@@ -11,11 +11,12 @@ from pandas.api.types import CategoricalDtype
 import matplotlib.pyplot as plt
 from matplotlib import colors as c
 import numpy as np
+import seaborn as sns
 
 #df = pd.read_csv('/Users/pesa/Google Drive/dataVis2017/Group_Survey.csv')
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/1ym9TjJ7Yftu-AGzNXqCKfqlzLF6efN_qDaNXKYfniA8/gviz/tq?tqx=out:csv')
 
-# --- SORTING DATA TYPES ---
+# --- SORTING DATA TYPES & CLEANING UP ---
 
 # make timestamp into a datetime64 dtype
 df['Timestamp'] = pd.to_datetime(df['Timestamp'])
@@ -59,6 +60,7 @@ df['lwg_change'] = df['lwg_change'].map(boolMap)
 male_df = df[(df['gender'].isin(['Male']))]
 female_df = df[(df['gender'].isin(['Female']))]
 other_df = df[(df['gender'].isin(['Other']))]
+pnsgen_df = df[(df['gender'].isin(['Prefer not to say']))]
 
 # age
 teen_df = df[(df['age'].isin(['16-23']))]
@@ -74,9 +76,11 @@ latino_df = df[(df['ethnicity'].isin(['Latino-American']))]
 asian_df = df[(df['ethnicity'].isin(['Asian or Pacific Islander']))]
 brit_df = df[(df['ethnicity'].isin(['White British']))]
 wother_df = df[(df['ethnicity'].isin(['White Other']))]
+pnsetn_df = df[(df['ethnicity'].isin(['Prefer not to say']))]
 
 # --- SPLIT MUTLIPLE ANSWER FIELDS INTO NEW DATAFRAMES ---
 
+# define function to split appropriate fields in the input dataframe
 def splitMultiAns (dataFrame) :
     # create new data frames to store multiple answer elements of given dataframe
     # we do this by by splitting the "string" that contains the multiple answers
@@ -89,50 +93,171 @@ def splitMultiAns (dataFrame) :
     plat_count = pd.value_counts(platDF.values.flatten())
     discover_count = pd.value_counts(discoverDF.values.flatten())
     lwg_count = pd.value_counts(lwgDF.values.flatten())
+    # and we do the same again but with normalised values (percentages)
+    ways_countN = pd.value_counts(waysDF.values.flatten(), normalize=True)
+    plat_countN = pd.value_counts(platDF.values.flatten(), normalize=True)
+    discover_countN = pd.value_counts(discoverDF.values.flatten(), normalize=True)
+    lwg_countN = pd.value_counts(lwgDF.values.flatten(), normalize=True)
     # we return a dictionary that holds these series
     temp_dict = {'ways' : ways_count, 'plat' : plat_count,  
-      'discover' : discover_count, 'lwg' : lwg_count,}
+      'discover' : discover_count, 'lwg' : lwg_count,
+      'waysN' : ways_countN, 'platN' : plat_countN, 
+      'discoverN' : discover_countN, 'lwgN' : lwg_countN }
     return temp_dict
 
+# create dictionary containing the 4 series of counted elements in the global df
 global_multiAns = splitMultiAns(df)
 #print(global_multiAns['ways'])
 
-male_multiAns = splitMultiAns(male_df)
-female_multiAns = splitMultiAns(female_df)
-other_multiAns = splitMultiAns(other_df)
+#
+def countAllElements(sub_dictionary, global_dictionary) :
+    sub_dictionary['ways'] = (sub_dictionary['ways'].add(
+            global_dictionary['ways'])).subtract(global_dictionary['ways'])
+    sub_dictionary['plat'] = (sub_dictionary['plat'].add(
+            global_dictionary['plat'])).subtract(global_dictionary['plat'])
+    sub_dictionary['discover'] = (sub_dictionary['discover'].add(
+            global_dictionary['discover'])).subtract(global_dictionary['discover'])
+    sub_dictionary['lwg'] = (sub_dictionary['lwg'].add(
+            global_dictionary['lwg'])).subtract(global_dictionary['lwg'])
+    sub_dictionary['waysN'] = (sub_dictionary['waysN'].add(
+            global_dictionary['ways'])).subtract(global_dictionary['ways'])
+    sub_dictionary['platN'] = (sub_dictionary['platN'].add(
+            global_dictionary['plat'])).subtract(global_dictionary['plat'])
+    sub_dictionary['discoverN'] = (sub_dictionary['discoverN'].add(
+            global_dictionary['discover'])).subtract(global_dictionary['discover'])
+    sub_dictionary['lwgN'] = (sub_dictionary['lwgN'].add(
+            global_dictionary['lwg'])).subtract(global_dictionary['lwg'])
+    return sub_dictionary
+    
+# create dictionaries for the gender specific sub-dataframes
+# and make sure that they report a value (even 0) for all the answers
+# expressed in the global dataframe    
+male_multiAns = countAllElements(splitMultiAns(male_df), global_multiAns)
+female_multiAns = countAllElements(splitMultiAns(female_df), global_multiAns)
+other_multiAns = countAllElements(splitMultiAns(other_df), global_multiAns)
+pnsgen_multiAns = countAllElements(splitMultiAns(pnsgen_df), global_multiAns)
 
-teen_multiAns = splitMultiAns(teen_df)
-twenties_multiAns = splitMultiAns(twenties_df)
-thirties_multiAns = splitMultiAns(thirties_df)
-plusfifty_multiAns = splitMultiAns(plusfifty_df)
+teen_multiAns = countAllElements(splitMultiAns(teen_df), global_multiAns)
+twenties_multiAns = countAllElements(splitMultiAns(twenties_df), global_multiAns)
+thirties_multiAns = countAllElements(splitMultiAns(thirties_df), global_multiAns)
+plusfifty_multiAns = countAllElements(splitMultiAns(plusfifty_df), global_multiAns)
+
+carib_multiAns = countAllElements(splitMultiAns(carib_df), global_multiAns)
+black_multiAns = countAllElements(splitMultiAns(black_df), global_multiAns)
+latino_multiAns = countAllElements(splitMultiAns(latino_df), global_multiAns)
+asian_multiAns = countAllElements(splitMultiAns(asian_df), global_multiAns)
+brit_multiAns = countAllElements(splitMultiAns(brit_df), global_multiAns)
+wother_multiAns = countAllElements(splitMultiAns(wother_df), global_multiAns)
+pnsetn_multiAns = countAllElements(splitMultiAns(pnsetn_df), global_multiAns)
+pnsetn_multiAns = countAllElements(splitMultiAns(pnsetn_df), global_multiAns)
+
 
 # ------------------------
 
-print('-----')
-
-
-# create bar chart (histogram) along x axisto represent ways in which 
-# people listen to music the most
-ax = global_multiAns['ways'].plot.bar(rot=90)
-plt.tight_layout()
-
-# ------------------------
+# Pie chart showing whether taste has changed
 
 # sum all responses to the question:
 # "Has your most-listened music genre changed since to a year ago?"
 lwgc = pd.value_counts(df['lwg_change'].values.flatten())
+lwgc_male = pd.value_counts(male_df['lwg_change'].values.flatten())
+lwgc_female = pd.value_counts(female_df['lwg_change'].values.flatten())
+lwgc_other = pd.value_counts(other_df['lwg_change'].values.flatten())
 
 explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Yes')
-fig1, ax1 = plt.subplots() # Create a figure (top level container for all plot elements) and a set of subplots (axes)
-ax1.pie(lwgc,
-        explode=explode, autopct='%1.1f%%',
-        shadow=True, startangle=90) # set subplot 'ax1' as a pie plot
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-ax1.legend(loc=2, labels=['yes','no']) # plot legend
-ax1.set_title('Has your most-listened music genre changed since to a year ago?', fontweight="bold", size=10)
-plt.tight_layout() # make sure the text is not drawn outside of the window
+fig1, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2) # Create a figure (top level container for all plot elements) and a set of subplots (axes)
+
+def pieFunc1 (subplot, group, title='', inlabels=['yes','no']) :
+    subplot.pie(group,
+            explode=explode, autopct='%1.1f%%',
+            shadow=True, startangle=90) # set subplot 'ax1' as a pie plot
+    subplot.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    subplot.legend(loc=2, labels=inlabels) # plot legend
+    subplot.set_title(title, size=10)
+
+pieFunc1(ax1, lwgc, 'global')
+pieFunc1(ax2, lwgc_male, 'male')
+pieFunc1(ax3, lwgc_female, 'female')
+pieFunc1(ax4, lwgc_other, 'other')
+
+fig1.suptitle('Has your most-listened music genre changed since to a year ago?', 
+              y=1.0, fontweight="bold", size=10)
+fig1.set_tight_layout(True) # make sure the text is not drawn outside of the window
+fig1.set_size_inches(8, 6)
+
 # ------------------------
 
-#avoid labels to be cut out of the image
+# create Stacked bar chart (histogram) along x axis to represent ways in which 
+# people listen to music the most (divided by gender)
+
+ind = np.arange(len(global_multiAns['ways'])) # the x locations for the groups
+width = 0.45       # the width of the bars: can also be len(x) sequence
+
+fig2, (ax21, ax22, ax23) = plt.subplots(1, 3) # create a figure (window-object) with 3 subplots
+
+# create all the various bar charts (using 'bottom' attribute to stack them) [normalised]
+ax21.bar(ind, female_multiAns['waysN'], width, color = '#d62728')
+ax21.bar(ind, male_multiAns['waysN'], width, bottom=female_multiAns['waysN'],
+             color = '#0589f5')         
+ax21.bar(ind, other_multiAns['waysN'], width, 
+             bottom = female_multiAns['waysN'].add(male_multiAns['waysN']), 
+             color = '#eb05c8')
+ax21.bar(ind, pnsgen_multiAns['waysN'], width, 
+             bottom = female_multiAns['waysN'].add(male_multiAns['waysN']).add(
+                     other_multiAns['waysN']), color = '#eb5d05')
+             
+ax21.legend(loc=2, labels=('Female', 'Male', 'Other')) # plot legend
+
+# set subplot details
+ax21.set_xticks(ind)
+ax21.set_yticks(np.arange(0,140,10)/100)
+ax21.set_xticklabels(male_multiAns['ways'].index, rotation='vertical')
+ax21.set_ylabel('% of people within group')
+ax21.set_title('Ways of listening to music (%)')
+ax21.legend(loc = 'upper left')
+
+# create all the various bar charts (using 'bottom' attribute to stack them) [real vals]
+p1 = ax22.bar(ind, female_multiAns['ways'], width, color = '#d62728')
+p1.set_label('Female')
+p2 = ax22.bar(ind, male_multiAns['ways'], width, bottom=female_multiAns['ways'],
+             color = '#0589f5')
+p2.set_label('Male')
+p3 = ax22.bar(ind, other_multiAns['ways'], width, 
+             bottom = female_multiAns['ways'].add(male_multiAns['ways']), 
+             color = '#eb05c8')
+p3.set_label('Other')
+p4 = ax22.bar(ind, pnsgen_multiAns['ways'], width, 
+             bottom = female_multiAns['ways'].add(male_multiAns['ways']).add(
+                     other_multiAns['ways']), color = '#eb5d05')
+p4.set_label('Other')
+
+# set subplot details
+ax22.set_xticks(ind)
+ax22.set_yticks(np.arange(0,100,5))
+ax22.set_xticklabels(male_multiAns['ways'].index, rotation='vertical')
+ax22.set_ylabel('no of people')
+ax22.set_title('Ways of listening to music (count)')
+ax22.legend(loc = 'upper left')
+
+# add a Pie showing the gender division of the survey 
+gen = pd.value_counts(df['gender'].values.flatten())
+explode2 = (0.1, 0, 0, 0)
+ax23.pie(gen,
+        explode=explode2, autopct='%1.1f%%',
+        shadow=True, startangle=90, colors = ['#d62728', '#0589f5','#eb05c8','#eb5d05']) # set subplot 'ax1' as a pie plot
+ax23.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax23.legend(loc=2, labels=['Female','Male', 'Other', 'Prefer not to say']) # plot legend
+ax23.set_title('% of each group in tot. sample')
+
+
+fig2.set_size_inches(14, 6)
+fig2.set_tight_layout(True) #avoid labels to be cut out of the image
+             
+print('-----')
+
+# ------------------------
+
+
+
+# ------------------------
 
 plt.show()
